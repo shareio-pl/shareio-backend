c08210a4307a82e0694465d0eb06decbdf1db160
Jakub Kalinowski <jakubkalinowski1234@gmail.com>
2024-03-27 18:09:53 +0100

    Created custom exception structure (#2)

---
+++ lib/src/main/java/org/shareio/backend/exceptions/ShareioException.java
@@ -0,0 +1,7 @@
+package org.shareio.backend.exceptions;
+
+public class ShareioException extends Exception {
+    public ShareioException(String message) {
+        super(message);
+    }
+}
---
+++ lib/src/main/java/org/shareio/backend/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package org.shareio.backend.exceptions;
+
+public class ValidationException extends ShareioException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
