2f68de6429097c21104c683f189404fb1557795d
Sedarius-1 <jakubkalinowski1234@gmail.com>
2024-03-27 18:38:47 +0100

    Create Const

---
+++ lib/src/main/java/org/shareio/backend/Const.java
@@ -0,0 +1,8 @@
+package org.shareio.backend;
+
+public class Const {
+    public static final Integer minNameLength = 2;
+    public static final Integer maxNameLength = 20;
+    public static final String multipleValidationErrorCode = "MulValErr";
+    public static final String noSuchElementErrorCode = "NoElemErr";
+}
