11d801274c2ffc743236b31da1f914b32338eb5e
Tomasz Kowalczyk <kowalczyk.tomek11@gmail.com>
2024-05-06 15:41:35 +0200

    Quick small itsy bitsy fix

--- score/src/main/java/org/shareio/backend/core/model/ReviewSnapshot.java
+++ score/src/main/java/org/shareio/backend/core/model/ReviewSnapshot.java
@@ -4,7 +4,7 @@ import org.shareio.backend.core.model.vo.ReviewId;
 
 import java.time.LocalDateTime;
 
-public record ReviewSnapshot(ReviewId reviewId, Offer offer, Integer value, LocalDateTime date) {
+public record ReviewSnapshot(ReviewId reviewId, Offer offer, Float value, LocalDateTime date) {
     public ReviewSnapshot(Review review) {
         this(review.getReviewId(), review.getOffer(), review.getValue(), review.getDate());
     }
