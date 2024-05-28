ae811bd4bf21f6b70cad57bd70ab67559125afd4
Sedarius-1 <jakubkalinowski1234@gmail.com>
2024-05-28 11:55:04 +0200

    Logging in DeleteRESTController

--- bootstrap/src/main/java/org/shareio/backend/infrastructure/controller/DeleteRESTController.java
+++ bootstrap/src/main/java/org/shareio/backend/infrastructure/controller/DeleteRESTController.java
@@ -1,5 +1,6 @@
 package org.shareio.backend.infrastructure.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.shareio.backend.Const;
 import org.shareio.backend.controller.responses.CorrectResponse;
@@ -7,6 +8,7 @@ import org.shareio.backend.controller.responses.ErrorResponse;
 import org.shareio.backend.core.usecases.port.dto.RemoveResponseDto;
 import org.shareio.backend.core.usecases.port.in.*;
 import org.shareio.backend.exceptions.MultipleValidationException;
+import org.shareio.backend.security.RequestLogHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -32,31 +34,39 @@ public class DeleteRESTController {
     RemoveOfferUseCaseInterface removeOfferUseCaseInterface;
 
     @RequestMapping(value = "/user/delete/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> deleteUser(@PathVariable(value = "id") UUID id) {
+    public ResponseEntity<Object> deleteUser(HttpServletRequest httpRequest, @PathVariable(value = "id") UUID id) {
         try{
+            RequestLogHandler.handleRequest(httpRequest);
             getUserProfileUseCase.getUserProfileResponseDto(id);
         } catch (MultipleValidationException e) {
+            RequestLogHandler.handleErrorResponse(httpRequest,HttpStatus.FAILED_DEPENDENCY, "User with id: "+id+" is malformed");
             return new ErrorResponse(e.getErrorMap(), e.getMessage(), HttpStatus.FAILED_DEPENDENCY);
         } catch (NoSuchElementException e) {
+            RequestLogHandler.handleErrorResponse(httpRequest,HttpStatus.NOT_FOUND, "User with id: "+id+" not found");
             return new ErrorResponse(Const.noSuchElementErrorCode, HttpStatus.NOT_FOUND);
         }
         RemoveResponseDto removeResponseDto = new RemoveResponseDto();
         removeResponseDto = removeOffersForUserUseCaseInterface.removeOffersForUser(id, removeResponseDto);
         removeResponseDto = removeUserUseCaseInterface.removeUser(id, removeResponseDto);
+        RequestLogHandler.handleCorrectResponse(httpRequest);
         return new CorrectResponse(removeResponseDto, Const.successErrorCode, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/offer/delete/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> deleteOffer(@PathVariable(value = "id") UUID id) {
+    public ResponseEntity<Object> deleteOffer(HttpServletRequest httpRequest, @PathVariable(value = "id") UUID id) {
         try{
+            RequestLogHandler.handleRequest(httpRequest);
             getOfferUseCaseInterface.getOfferResponseDto(id, null, null);
         } catch (MultipleValidationException e) {
+            RequestLogHandler.handleErrorResponse(httpRequest,HttpStatus.FAILED_DEPENDENCY, "Offer with id: "+id+" is malformed");
             return new ErrorResponse(e.getErrorMap(), e.getMessage(), HttpStatus.FAILED_DEPENDENCY);
         } catch (NoSuchElementException e) {
+            RequestLogHandler.handleErrorResponse(httpRequest,HttpStatus.NOT_FOUND, "Offer with id: "+id+" not found");
             return new ErrorResponse(Const.noSuchElementErrorCode, HttpStatus.NOT_FOUND);
         }
         RemoveResponseDto removeResponseDto = new RemoveResponseDto();
         removeResponseDto = removeOfferUseCaseInterface.removeOffer(id, removeResponseDto);
+        RequestLogHandler.handleCorrectResponse(httpRequest);
         return new CorrectResponse(removeResponseDto, Const.successErrorCode, HttpStatus.OK);
     }
 }
