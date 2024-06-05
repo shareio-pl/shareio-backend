7660debb09ca1c89205360ef2cd7ba471ebfaaec
Jakub Kalinowski <jakubkalinowski1234@gmail.com>
2024-06-05 11:44:22 +0200

    Distance fix (#68)

--- location/src/main/java/org/shareio/backend/core/usecases/util/DistanceCalculator.java
+++ location/src/main/java/org/shareio/backend/core/usecases/util/DistanceCalculator.java
@@ -1,13 +1,24 @@
 package org.shareio.backend.core.usecases.util;
 
-import org.locationtech.jts.geom.Coordinate;
+//import org.locationtech.jts.geom.Coordinate;
 import org.shareio.backend.core.model.vo.Location;
 
 public class DistanceCalculator {
+//    public static Double calculateDistance(Location l1, Location l2) {
+//        Coordinate point1 = new Coordinate(l1.getLatitude(), l1.getLongitude());
+//        Coordinate point2 = new Coordinate(l2.getLatitude(), l2.getLongitude());
+//        return point1.distance(point2) / 1000; // .distance() returns distance in meters
+//    }
+
     public static Double calculateDistance(Location l1, Location l2) {
-        Coordinate point1 = new Coordinate(l1.getLatitude(), l1.getLongitude());
-        Coordinate point2 = new Coordinate(l2.getLatitude(), l2.getLongitude());
-        return point1.distance(point2) / 1000; // .distance() returns distance in meters
+        double lat1Rad = Math.toRadians(l1.getLatitude());
+        double lat2Rad = Math.toRadians(l2.getLatitude());
+        double lon1Rad = Math.toRadians(l1.getLongitude());
+        double lon2Rad = Math.toRadians(l2.getLongitude());
+
+        double x = (lon2Rad - lon1Rad) * Math.cos((lat1Rad + lat2Rad) / 2);
+        double y = (lat2Rad - lat1Rad);
+        return Math.sqrt(x * x + y * y) * 6371;
     }
 
     private DistanceCalculator() {
